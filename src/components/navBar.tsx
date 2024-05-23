8578ea46334698d5924a743e0cb89400dc3058db
Joao Carlos <joaocpinheiro03@gmail.com>
2024-05-23 19:15:19 -0300

    feat: navBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { FormEvent } from 'react'
 
@@ -15,8 +16,25 @@ export function NavBar() {
   }
 
   return (
-    <form onSubmit={handleSearch}>
-      <input placeholder="Buscar Filme..." id="q" name="q" type="text" />
-    </form>
+    <div className="bg-primary">
+      <div className="flex justify-between items-center py-4 px-2 md:px-10">
+        <Link className="hidden md:block" href="/discover/now_playing">
+          <h2 className="text-[30px]">Movieverse</h2>
+        </Link>
+
+        <form className="space-x-4" onSubmit={handleSearch}>
+          <input
+            placeholder="Buscar Filme..."
+            id="q"
+            name="q"
+            type="text"
+            className="bg-secondary px-4 py-2 outline-none placeholder:text-textColor rounded-md"
+          />
+          <button className="bg-secondary text-textColor py-2 px-4 hover:bg-textColor hover:text-white rounded-md">
+            Buscar
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
